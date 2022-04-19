dfd9ecbcec3fae66f20207dccb75ec67a7a1c23d
chidentz <chidentzovinsky@gmail.com>
2022-04-19 15:33:43 -0500

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       final byte MONTHS_IN_YEAR = 12;
-       final byte Percent = 100;
-         Scanner scanner = new Scanner(System.in);
-        System.out.println("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / Percent / MONTHS_IN_YEAR;
-        System.out.println("Period(years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-        double mortgage = principal
-                   * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments));
-         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-         System.out.println("Mortgage: " + mortgageFormatted);
+      boolean hasHighincome = false;
+        System.out.println("No");
     }
 }
