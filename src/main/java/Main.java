01ebf3900b4ad6e6ec07c573529d233a5d255572
chidentz <chidentzovinsky@gmail.com>
2022-04-19 12:22:37 -0500

    Arrays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       String message = "Hello World";
-        System.out.println(message.indexOf("H"));
+      int [] numbers = {2,3,5,1,4};
+      Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
