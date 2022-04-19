c9a69fc93f134955b2b136a2f6cf5f01f750d582
chidentz <chidentzovinsky@gmail.com>
2022-04-19 13:40:57 -0500

    Add currency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-   int result = (int) Math.round(Math.random() * 100) ;
+        String result=NumberFormat.getCurrencyInstance().format(1234567.891);
         System.out.println(result);
     }
 }
