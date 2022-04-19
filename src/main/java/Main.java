912e6f1f624407c6006181560003e7135c031e11
chidentz <chidentzovinsky@gmail.com>
2022-04-19 13:12:10 -0500

    Random numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-   short x = 1;
-   int y = x + 2;
-        System.out.println(y);
+   int result = (int) Math.round(Math.random() * 100) ;
+        System.out.println(result);
     }
 }
