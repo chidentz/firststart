0c5b01f445e622f6b176a6e62c27c5b35817eb2e
chidentz <chidentzovinsky@gmail.com>
2022-04-20 10:14:04 -0500

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      boolean hasHighincome = false;
-        System.out.println("No");
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if (temp > 20)
+            System.out.println("Beautiful day");
+     else
+        System.out.println("Cold day");
     }
-}
+   {
+
+   }
+    }
