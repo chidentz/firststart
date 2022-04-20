d4eb67afdcd8cc28212d91fc54d938e820b62984
chidentz <chidentzovinsky@gmail.com>
2022-04-20 10:48:45 -0500

    Switch statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int temp = 32;
-        if (temp > 30) {
-            System.out.println("It's a hot day");
-            System.out.println("Drink water");
-        }
-        else if (temp > 20)
-            System.out.println("Beautiful day");
-     else
-        System.out.println("Cold day");
-    }
-   {
+      String role = "moderator";
+              switch (role){
+                  case "admin":
+                      System.out.println("You're an admin");
+                              break;
+                  case "moderator":
+                      System.out.println("You're a moderator");
+                              break;
+                  default:
+                      System.out.println("You're a guest");
+              }
 
    }
     }
